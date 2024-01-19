34cc7510492159d121dd01790290ac212b2dc9b4
joel c <150722950+jo3l4@users.noreply.github.com>
2024-01-19 08:55:46 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,79 @@
+// Complete the following functions to make our program work!
+
+/**
+ * Takes a F number and returns the number to C
+ * @param {string} fahren temperature in degrees F
+ * @returns {number} the number of degrees C
+ */
+function convertToCelsius(fahren) {}
+
+/**
+ * Takes both numbers (F, C) and display a message with
+ * both numbers and how that temp makes you feel using
+ * this chart
+ *
+ *   Temp      Feels
+ * ---------|----------
+ *   < 32   |  "very cold"
+ *   < 64   |  "cold"
+ *   < 86   |  "warm"
+ *   < 100  |  "hot"
+ *
+ * @param {number} fahren
+ * @param {number} celc
+ */
+function convertToCelsius(fahrenheit) {
+  return (fahrenheit - 32) * 5/9;
+}
+
+function createMessage(fahrenheit, celsius) {
+  if (fahrenheit < 32) {
+    return `Temperature: ${fahrenheit}°F (${celsius}°C) - Feels very cold`;
+  } else if (fahrenheit < 64) {
+    return `Temperature: ${fahrenheit}°F (${celsius}°C) - Feels cold`;
+  } else if (fahrenheit < 86) {
+    return `Temperature: ${fahrenheit}°F (${celsius}°C) - Feels warm`;
+  } else {
+    return `Temperature: ${fahrenheit}°F (${celsius}°C) - Feels hot`;
+  }
+}
+
+function rand(limit) {
+  return Math.floor(Math.random() * (limit + 1));
+}
+
+
+
+
+/**
+ * Takes a number and returns a random integer from 0 to the limit
+ * @param {number} limit
+ * @returns {number} a number between 0 and the int passed in
+ */
+function rand(limit) {}
+
+// -------------------- DO NOT CHANGE THE CODE BELOW ---------------------- //
+
+let fahren = prompt(
+  "enter a number, we will convert that number from fahrenheit to celcius"
+);
+let celc = convertToCelsius(fahren);
+let output = createMessage(fahren, celc);
+console.log(output);
+
+fahren = prompt(
+  "Lets try that again. enter a number, we will convert that number from fahrenheit to celcius"
+);
+celc = convertToCelsius(fahren);
+output = createMessage(fahren, celc);
+console.log(output);
+
+fahren = rand(110);
+celc = convertToCelsius(fahren);
+output = createMessage(fahren, celc);
+console.log(output);
+
+fahren = rand(110);
+celc = convertToCelsius(fahren);
+output = createMessage(fahren, celc);
+console.log(output);
